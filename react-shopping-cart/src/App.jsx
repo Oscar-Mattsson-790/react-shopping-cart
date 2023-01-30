8099bf78dbb7aff18f63f7b386c88ef1b0d3b85f
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-01-30 16:42:08 +0700

    Complete

--- react-shopping-cart/src/App.jsx
+++ react-shopping-cart/src/App.jsx
@@ -3,11 +3,52 @@ import Product from "./components/Product/Product";
 import Header from "./components/Header/Header";
 
 function App() {
+  const books = [
+    {
+      id: 0,
+      title: "A Sign of Four",
+      author: "Av Sir Arthur Conan Doyle",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolorum voluptatem eligendi quidem nostrum suscipit magnam delectus quam totam consequatur, unde corporis iure facilis laudantium. Expedita consequatur aliquam quod autem.",
+    },
+    {
+      id: 1,
+      title: "A Study in Scarlet",
+      author: "Av Sir Arthur Conan Doyle",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolorum voluptatem eligendi quidem nostrum suscipit magnam delectus quam totam consequatur, unde corporis iure facilis laudantium. Expedita consequatur aliquam quod autem.",
+    },
+    {
+      id: 2,
+      title: "Baskervilles Hound",
+      author: "Av Sir Arthur Conan Doyle",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolorum voluptatem eligendi quidem nostrum suscipit magnam delectus quam totam consequatur, unde corporis iure facilis laudantium. Expedita consequatur aliquam quod autem.",
+    },
+    {
+      id: 3,
+      title: "The Adventures of Sherlock Holmes",
+      author: "Av Sir Arthur Conan Doyle",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolorum voluptatem eligendi quidem nostrum suscipit magnam delectus quam totam consequatur, unde corporis iure facilis laudantium. Expedita consequatur aliquam quod autem.",
+    },
+  ];
+
+  const booksComponents = books.map((book) => {
+    return (
+      <Product
+        title={book.title}
+        author={book.author}
+        text={book.text}
+        key={book.id}
+      />
+    );
+  });
+
+  console.log(books);
+
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
-        <Product
+        {booksComponents}
+        {/* <Product
           title="A Sign of Four"
           author="Av Sir Arthur Conan Doyle"
           text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
@@ -38,7 +79,7 @@ function App() {
         dolorum voluptatem eligendi quidem nostrum suscipit magnam delectus quam
         totam consequatur, unde corporis iure facilis laudantium. Expedita
         consequatur aliquam quod autem."
-        />
+        /> */}
       </div>
     </div>
   );
