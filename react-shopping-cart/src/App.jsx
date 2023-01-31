f232f50d01119397cba361189912c45d811cc791
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-01-31 09:05:30 +0700

    Complete

--- react-shopping-cart/src/App.jsx
+++ react-shopping-cart/src/App.jsx
@@ -41,45 +41,11 @@ function App() {
     );
   });
 
-  console.log(books);
-
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
         {booksComponents}
-        {/* <Product
-          title="A Sign of Four"
-          author="Av Sir Arthur Conan Doyle"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-        dolorum voluptatem eligendi quidem nostrum suscipit magnam delectus quam
-        totam consequatur, unde corporis iure facilis laudantium. Expedita
-        consequatur aliquam quod autem."
-        />
-        <Product
-          title="A Study in Scarlet"
-          author="Av Sir Arthur Conan Doyle"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-        dolorum voluptatem eligendi quidem nostrum suscipit magnam delectus quam
-        totam consequatur, unde corporis iure facilis laudantium. Expedita
-        consequatur aliquam quod autem."
-        />
-        <Product
-          title="Baskervilles Hound"
-          author="Av Sir Arthur Conan Doyle"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-        dolorum voluptatem eligendi quidem nostrum suscipit magnam delectus quam
-        totam consequatur, unde corporis iure facilis laudantium. Expedita
-        consequatur aliquam quod autem."
-        />
-        <Product
-          title="The Adventures of Sherlock Holmes"
-          author="Av Sir Arthur Conan Doyle"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-        dolorum voluptatem eligendi quidem nostrum suscipit magnam delectus quam
-        totam consequatur, unde corporis iure facilis laudantium. Expedita
-        consequatur aliquam quod autem."
-        /> */}
       </div>
     </div>
   );
